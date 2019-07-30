5ae2a4dc9dac73a9c8ba7657b8b6e132bd31f436
VincentXu <1657550124@qq.com>
2019-07-30 10:19:17 +0800

    11

--- Views/Controllers/ViewController5.m
+++ Views/Controllers/ViewController5.m
@@ -20,15 +20,15 @@
 }
 
 -(void)convertStringBetweenOCAndC{
+    //先把oc转c，再转回oc（待研究）
     //oc -> c
     NSString *ocString = @"This is a OC string";
     //静态方法
-    if (ocst) {
-        <#statements#>
+    if ([ocString canBeConvertedToEncoding:NSUTF8StringEncoding]) {
+        const char *cString1 = [ocString cStringUsingEncoding:NSUTF8StringEncoding];
+        const char *cString2 = [ocString UTF8String];
+        NSLog(@"\n\n1——%s\n\n2——%s",cString1,cString2);
     }
-    const char *cString1 = [ocString cStringUsingEncoding:NSUTF8StringEncoding];
-    const char *cString2 = [ocString UTF8String];
-    NSLog(@"\n\n1——%s\n\n2——%s",cString1,cString2);
     
     //c -> oc
     const char *cString = "This is a C string";
@@ -40,8 +40,22 @@
     NSString *ocString4 = [NSString stringWithCString:cString encoding:NSUTF8StringEncoding];
     NSString *ocString5 = [NSString stringWithUTF8String:cString];
     NSString *ocString6 = [NSString stringWithFormat:@"%s",cString];
-    [NSLog   (@"\n\n1——%@\n\n2——%@\n\n3——%@\n\n4——%@\n\n5——%@\n\n6——%@",ocString1,ocString2,ocString3,ocString4,ocString5,ocString6)];
-    [ocString1 release]; [ocString2 release];
+    NSLog   (@"\n\n1——%@\n\n2——%@\n\n3——%@\n\n4——%@\n\n5——%@\n\n6——%@",ocString1,ocString2,ocString3,ocString4,ocString5,ocString6);
+}
+
+-(void)inputOCStringURLEncode{
+    NSString *url = @"this is a 中文字符串、有 特殊格式。";
+    NSString *urlEncode =  [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"`#%^{}\"[]|\\<> "].invertedSet];
+    if(@available(iOS 9.0, *)){
+        urlEncode = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLPathAllowedCharacterSet]];
+        
+    }else{
+        urlEncode = (NSString*)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,(CFStringRef)url,NULL,(CFStringRef)@"!*'();:@&=+$,/?%#[]",kCFStringEncodingUTF8));
+    }
+    
+    NSString *decodedString  = (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL,
+    (__bridge CFStringRef)urlEncode,CFSTR(""),CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    
 }
 
 @end
